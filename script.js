10a6ce704fef101ea2c4f3be5cc407cfedf4d89e
Kareem Zidane <kzidane@cs50.harvard.edu>
2018-11-02 07:15:33 -0400

    enabled AI for bigint

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function _toggleAutoIncrement(i) {
     if (!autoIncrement)
         return false;
 
-    if (selectedOption == "integer" && document.querySelector(`#i${i}_primarykey`).checked) {
+    if (selectedOption == "integer" || selectOption == "bigint" && document.querySelector(`#i${i}_primarykey`).checked) {
         autoIncrement.disabled = false;
     }
     else {
